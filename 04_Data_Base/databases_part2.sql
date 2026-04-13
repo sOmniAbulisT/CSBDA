63589f82e8cca7ccc26e25b9afed6c16b475c3e6
KunHong Liao <r13621207@ntu.edu.tw>
2026-04-13 16:05:24 +0800

    260413 1604

---
+++ 04_Data_Base/databases_part2.sql
@@ -0,0 +1,20 @@
+-- Combines rows from two or more tables in databases
+SELECT * FROM Seed_Info 
+	JOIN Company_Info 
+	ON Seed_Info.Company_id = Company_Info = Company_ID; -- use Company_ID/Comapny_id to join 
+
+-- Combines rows from two or more tables in databases with target columns
+SELECT Hybrid_name, Maturity, Company_name, Parent_name
+FROM Seed_Info JOIN Company_Info
+ON Seed_Info.Company_id = Company_Info = Company_ID; 
+
+-- Aggregate function: Count
+SELECT COUNT(*) FROM Seed_Info; -- calculate # of rows
+SELECT COUNT(*) FROM State_Info; 
+
+SELECT COUNT(*) FROM State_Info JOIN Farm_Info
+ON State_Info.State_ID = Farm_Info.State_ID; 
+
+-- Aggregate function: DISTINCT
+
+
